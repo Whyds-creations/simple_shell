ef50c5fb860ee231175aef4968b8a364b416b37f
Tobzid <105335887+Tobzid@users.noreply.github.com>
2022-09-07 22:36:48 +0100

    Update env_management.c

--- env_management.c
+++ env_management.c
@@ -2,7 +2,7 @@
 
 /**
  * env_get_key - gets the value of an environment variable
- * @key: the environment variable of interest
+ * @key: the environment variable of interest...
  * @data: struct of the program's data
  * Return: a pointer to the value of the variable or NULL if it doesn't exist
  */
